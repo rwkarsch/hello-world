0acc707a4e71e2c18d824ceb50c7f50be1fc41e9
Bob Karsch <robertkarsch@gmail.com>
2017-03-04 08:06:13 -0500

    first commit of hello world

--- src/index.js
+++ src/index.js
@@ -11,10 +11,31 @@ var handlers = {
     'LaunchRequest': function () {
         this.emit('SayHello');
     },
-    'HelloWorldIntent': function () {
+    
+    'BobWorldIntent': function () {
         this.emit('SayHello')
     },
+    
+    'FeelingIntent': function () {
+        this.emit('Feeling')
+    },   
+
+    'IntroIntent': function () {
+        this.emit('Intro')
+    },   
+    
     'SayHello': function () {
-        this.emit(':tell', 'Hello World!');
+        this.emit(':tell', 'Hello Ben Rodd, how are you today? Are you ready for your gig?');
+    },
+    
+    'Feeling': function () {
+        this.emit(':tell', 'I feel GREAT! How about you?');
+    },
+    
+    'Intro': function () {
+        
+
+        console.log('I am in the intro intent')
+        this.emit(':tell', 'Gladly! <audio src="https://s3.amazonaws.com/rwkarschbucket/Tequila_10_Seconds.mp3" /> Gladly! Ladies and Gentlemen, Boys and Girls - you attention please!!! Introducing, the one, the only: Ben Rodd');
     }
 };
